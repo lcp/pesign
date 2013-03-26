e53c1f90ff60bcfbde195d5ec0db8b3c97d9e9c2
Gary Ching-Pang Lin <glin@suse.com>
2013-03-26 18:30:58 +0800

    Clear the space for the certificate list
    Make sure the aligned bytes are '\0'
    
    Signed-off-by: Gary Ching-Pang Lin <glin@suse.com>

--- src/wincert.c
+++ src/wincert.c
@@ -35,7 +35,7 @@ generate_cert_list(SECItem **signatures, int num_signatures,
 		cl_size += ALIGNMENT_PADDING(cl_size, 8);
 	}
 
-	uint8_t *data = malloc(cl_size);
+	uint8_t *data = calloc(1, cl_size);
 	if (!data)
 		return -1;
 
